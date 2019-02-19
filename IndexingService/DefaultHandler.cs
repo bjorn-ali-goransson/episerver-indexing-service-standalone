48f9f94083c13ae698455ae6e42bd15d2f4035dd
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-02-19 22:42:07 +0100

    Initial commit

---
+++ IndexingService/DefaultHandler.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Logging.Compatibility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace IndexingService
+{
+    public class DefaultHandler : IHttpHandler
+    {
+        public bool IsReusable => false;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("IndexingService");
+        }
+    }
+}
